50b46c6466b8f88cd1ebc0ecbcddf3b0fefa401d
arekgazdziak <arekgazdziak@gmail.com>
2015-03-11 00:44:14 +0100

    Admin p2

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,6 +2,6 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
   	@user = User.from_omniauth(request.env["omniauth.auth"])
   	sign_in_and_redirect @user
-  	current_user.update_attribute :admin, true
+  
   end
 end
