a8633778d22ae65146eb1a8228a83b3fbf36e217
arekgazdziak <arekgazdziak@gmail.com>
2015-03-11 01:03:31 +0100

    Added 'Hi' element, to the menu

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,7 +1,10 @@
 class CallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
   	@user = User.from_omniauth(request.env["omniauth.auth"])
-  	sign_in_and_redirect @user
-  
+  	sign_in_and_redirect @user  
+  end
+
+  def log_out
+    sign_out(@user)
   end
 end
