021073f7e7620269a627a78259db377306795e8f
arekgazdziak <arekgazdziak@gmail.com>
2015-04-19 21:32:12 +0200

    Back slides

--- app/assets/javascripts/resumeBuilder.js
+++ app/assets/javascripts/resumeBuilder.js
@@ -1,2 +1,33 @@
-// $('.portfolio-header').load(	$('.portfolio-header').hide('slow').show(3000));
+$("#arrow").click(function() {
+  window.location.href = '#main';
+});
 
+var currentIndex = 0;
+var items = $('.portfolio-header');
+var itemAmt = items.length;
+
+cycleItems();
+
+function cycleItems() {
+  var item = $('.portfolio-header').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+$('.next').click(function() {
+  currentIndex += 1;
+
+  
+
+  cycleItems();
+
+});
+
+$('.prev').click(function() {
+  currentIndex -= 1;
+
+  
+
+  cycleItems();
+
+});
