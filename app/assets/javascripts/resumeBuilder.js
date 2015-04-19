6e056689232726a7887496b20409a4a58c3800ff
arekgazdziak <arekgazdziak@gmail.com>
2015-04-19 22:38:46 +0200

    header phase 144

--- app/assets/javascripts/resumeBuilder.js
+++ app/assets/javascripts/resumeBuilder.js
@@ -2,11 +2,34 @@ $("#arrow").click(function() {
   window.location.href = '#main';
 });
 
+
+    /*<nav id="nav-dots" class="nav-dots">
+      <span class="nav-dot-current"></span>
+      <span></span>
+      <span></span>
+    </nav>*/
+
 var currentIndex = 0;
 var items = $('.portfolio-header');
 var itemAmt = items.length;
 
+var dots = $('.nav-dots span');
+
+
+
 cycleItems();
+
+function addDot() {
+
+    $('.nav-dots span').eq(currentIndex).addClass("nav-dot-current");
+
+}
+
+function removeDot() {
+
+    $('.nav-dots span').eq(currentIndex).removeClass("nav-dot-current");
+
+}
 
 function cycleItems() {
   var item = $('.portfolio-header').eq(currentIndex);
@@ -15,18 +38,32 @@ function cycleItems() {
 }
 
 $('.next').click(function() {
-  currentIndex += 1;
-
   
+	removeDot();
+
+  currentIndex += 1;
+  if ( currentIndex > itemAmt - 1 )
+  {
+      currentIndex = 0;
+  } 
+
+  addDot();
 
   cycleItems();
 
 });
 
 $('.prev').click(function() {
-  currentIndex -= 1;
 
+	removeDot();
   
+  currentIndex -= 1;
+  if ( currentIndex < 0 )
+  {
+      currentIndex = itemAmt - 1;
+  } 
+  
+  addDot();
 
   cycleItems();
 
