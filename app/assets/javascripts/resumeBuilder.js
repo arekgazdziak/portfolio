3cfc6088fbe398650d6a153e7a7063dde23c421d
arekgazdziak <arekgazdziak@gmail.com>
2015-04-26 20:14:06 +0200

    Embedded SC player

--- app/assets/javascripts/resumeBuilder.js
+++ app/assets/javascripts/resumeBuilder.js
@@ -2,12 +2,19 @@ $("#arrow").click(function() {
   window.location.href = '#main';
 });
 
+SC.initialize({
+  client_id: "69443d2b3a72b56dd44f849ed235735b"
+});
 
-    /*<nav id="nav-dots" class="nav-dots">
-      <span class="nav-dot-current"></span>
-      <span></span>
-      <span></span>
-    </nav>*/
+var track_url = "http://soundcloud.com/arek-gazdziak/little-wing-intro-005"
+
+$(document).ready(function() {
+
+  SC.oEmbed(track_url, document.getElementById('player'));
+  console.log(track_url);
+
+});
+
 
 var currentIndex = 0;
 var items = $('.portfolio-header');
