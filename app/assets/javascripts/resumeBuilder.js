51bd57ce14d7e0333fe02138bfa756511a92d78f
arekgazdziak <arekgazdziak@gmail.com>
2015-05-03 15:59:08 +0200

    JS

--- app/assets/javascripts/resumeBuilder.js
+++ app/assets/javascripts/resumeBuilder.js
@@ -1,7 +1,3 @@
-$("#arrow").click(function() {
-  window.location.href = '#main';
-});
-
 SC.initialize({
   client_id: "69443d2b3a72b56dd44f849ed235735b"
 });
@@ -15,86 +11,8 @@ $(document).ready(function() {
 
 });
 
-var currentIndex = 0;
-var items = $('.portfolio-header');
-var itemAmt = items.length;
-var dots = $('.nav-dots span');
-
-function addDot() {
-
-    $('.nav-dots span').eq(currentIndex).addClass("nav-dot-current");
-
-}
-
-function removeDot() {
-
-    $('.nav-dots span').eq(currentIndex).removeClass("nav-dot-current");
-
-}
-
-function cycleItems() {
-  var item = $('.portfolio-header').eq(currentIndex);
-  items.hide();
-  item.css('display','inline-block');
-}
-
-function nextSlide() {
-
-	removeDot();
-
-  currentIndex += 1;
-  if ( currentIndex > itemAmt - 1 )
-  {
-      currentIndex = 0;
-  } 
-
-  addDot();
-
-}
-
-function prevSlide() {
-
-	removeDot();
-  
-  currentIndex -= 1;
-  if ( currentIndex < 0 )
-  {
-      currentIndex = itemAmt - 1;
-  } 
-  
-  addDot();
-
-}
-
-$('.next').click(function() {
-  
-  nextSlide();
-  cycleItems();
-
-});
-
-$('.prev').click(function() {
-
-  prevSlide();
-  cycleItems();
-
-});
 
 
-$('#arrow')
-    .mouseover(function() {
-        $(this).animate({height: 70}, 500);
-    })
-    .mouseout(function() {
-        $(this).animate({height: 50}, 500);
-    });
 
-$('document').ready( function() {
 
-  if($('#main'))
-  {
-  	  console.log("arek");
-      cycleItems();      
-  }
 
-});
